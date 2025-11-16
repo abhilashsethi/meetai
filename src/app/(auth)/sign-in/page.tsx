1b0a9d241157aa9995ea13481ca42a6609bda952
abhilash <abhilashsethi9@gmail.com>
2025-11-16 07:53:57 +0530

    04: authentication ui

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import { Card } from "@/components/ui/card";
+import { SignInView } from "@/modules/auth/ui/views/sign-in-views";
+import React from "react";
+
+const Page = () => {
+  return <SignInView />;
+};
+
+export default Page;
