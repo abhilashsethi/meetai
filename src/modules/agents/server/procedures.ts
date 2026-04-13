dfa33967861c3243ebdcf3114f4b8e88a310f1c0
abhilash <abhilashsethi9@gmail.com>
2026-04-13 23:34:28 +0530

    14: agent page

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,23 +1,30 @@
+import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constant";
 import { db } from "@/db";
 import { agents } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { TRPCError } from "@trpc/server";
 import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 import { z } from "zod";
 import { agentsInsertSchema } from "../schemas";
-import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constant";
-import { pages } from "next/dist/build/templates/app-page";
 
 
 export const agentsRouter = createTRPCRouter({
   // TODO: change `getOne to use `protectedProcedure`
   getOne: protectedProcedure.input(z.object({
     id: z.string()
-  })).query(async ({ input }) => {
+  })).query(async ({ input, ctx }) => {
     const [existingAgent] = await db.select({
       // TODO change t actual count
       meetingCount: sql<number>`5`,
       ...getTableColumns(agents),
-    }).from(agents).where(eq(agents.id, input.id))
+    }).from(agents).where(and(
+      eq(agents.id, input.id),
+      eq(agents.userId, ctx.auth.user.id)
+    ))
+
+    if (!existingAgent) {
+      throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" })
+    }
 
     return existingAgent
   }),
